f885cba5d7da8907b6360b1d2419e9ce24de4b40
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-01-18 21:57:22 +0530

    tried to improve but nothng new

--- users/forms.py
+++ users/forms.py
@@ -12,3 +12,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'BITSID', 'mobile_number', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    mobile_number = forms.IntegerField()
+    BITSID = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'BITSID', 'mobile_number']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
