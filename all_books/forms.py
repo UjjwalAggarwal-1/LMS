d7ee79781d06c32e16475717782832d0b28353cd
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-06 00:43:49 +0530

    some changes in request book css

--- all_books/forms.py
+++ all_books/forms.py
@@ -30,6 +30,6 @@ class ReturnBookForm(forms.ModelForm):
     class Meta:
         model = Issue2
         fields = ['issue_request_status']
-        widgets = {'issue_reqeust_status': forms.Select(choices=[('ISSUED', 'YES'), ('NO', 'RETURNED'), ('UNKNOWN', 'UNKNOWN')])}
+        widgets = {'issue_reqeust_status': forms.ChoiceField(choices=(('ISSUED', 'YES'), ('NO', 'RETURNED'), ('UNKNOWN', 'UNKNOWN')))}
 
 
