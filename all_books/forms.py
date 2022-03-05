583bcfda64fe93f8bdd5aa31be3febab09e26ccf
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-05 23:18:08 +0530

    minor changes in issue and request css

--- all_books/forms.py
+++ all_books/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from all_books.models import Books
+from all_books.models import Books, Issue2
 import datetime
 
 
@@ -13,7 +13,6 @@ class RequestBookForm(forms.Form):
     now = datetime.date.today()
     return_on = forms.DateTimeField(widget=forms.SelectDateWidget(attrs={'min': now,
                                                                          'max': now + datetime.timedelta(days=20)}))
-    #isbn = forms.CharField(max_length=20)
 
 
 class AddBookForm(forms.ModelForm):
@@ -25,3 +24,12 @@ class AddBookForm(forms.ModelForm):
 class RequestDecisionForm(forms.Form):
     issue_request_status = forms.BooleanField(required=False)
     reject_request = forms.CharField(max_length=300, required=False)
+
+
+class ReturnBookForm(forms.ModelForm):
+    class Meta:
+        model = Issue2
+        fields = ['issue_request_status']
+        widgets = {'issue_reqeust_status': forms.Select(choices=[('ISSUED', 'YES'), ('NO', 'RETURNED'), ('UNKNOWN', 'UNKNOWN')])}
+
+
