80bee94fa0f4a09dd3598aace96458815602478d
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-06 18:40:08 +0530

    updated profile page

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
 from .models import Profile
-
-
-class UserUpdateForm(forms.ModelForm):
-    mobile_number = forms.IntegerField()
-    BITSID = forms.CharField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'BITSID', 'mobile_number']
 
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['BITSID', 'mobile_num']
--- users/views.py
+++ users/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserUpdateForm, ProfileUpdateForm
+from .forms import ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
+        if p_form.is_valid():
             p_form.save()
             messages.success(request, '!!Your account is now updated!!')
             return redirect('all_books_home')
     else:
-        u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {'title': 'Profile', 'u_form': u_form, 'p_form': p_form}
+    context = {'title': 'Profile', 'p_form': p_form}
 
     return render(request, 'users/profile.html', context)
 
