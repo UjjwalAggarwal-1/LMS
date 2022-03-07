48d12b6df158457e88c149afe4e6754e881532f1
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-07 12:20:55 +0530

    clarified names files and variables

--- all_books/admin.py
+++ all_books/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from.models import Books, Issue2
+from.models import Book, Issue
 
 
-admin.site.register(Books)
-admin.site.register(Issue2)
+admin.site.register(Book)
+admin.site.register(Issue)
 
 
