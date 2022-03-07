e2fd5b609c2e044233b23f039d85cc680ad7530d
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-08 04:28:22 +0530

    differentiate bw new and renew, latest books page

--- all_books/models.py
+++ all_books/models.py
@@ -13,17 +13,26 @@ class Book(models.Model):
     location = models.CharField(max_length=20, default='unknown')
     displayed_from = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now())
     availability = models.ManyToManyField(User, through='Issue')
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
 
 
+
+status_choices = [("pending", 'Pending'), ('issued', 'Issued'), ('returned', 'Returned'), ('rejected', 'Rejected'), ('renewed', 'Renewed')]
+rating_choices = [(0, 'No Rating'), (1, 'Poor'), (2, 'Average'), (3, 'Good'), (4, 'Very Good'), (5, 'Excellent')]
+
+
 class Issue(models.Model):
     isbn_of_book = models.ForeignKey(Book, on_delete=models.CASCADE)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-    return_on = models.DateField(null=True, default=datetime.date.today()+datetime.timedelta(days=20))
-    issue_request_status = models.CharField(max_length=10, choices=[
-                ("pending", 'Pending'), ('issued', 'Issued'), ('returned', 'Returned'), ('rejected', 'Rejected')
-                ], default='pending', null=True, blank=True)
+    issue_request_status = models.CharField(max_length=10, choices=status_choices, default='pending', null=True, blank=True)
     reject_request = models.CharField(max_length=300, null=True, blank=True)
+    return_on = models.DateField(null=True, default=datetime.date.today() + datetime.timedelta(days=20))
+    issued_on = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now())
+    returned_on = models.DateField(null=True, default=None)
+    renewed_on = models.DateField(null=True, default=None)
+    rating = models.IntegerField(choices=rating_choices, default=0, blank=True, null=True)
+    review = models.TextField(null=True, blank=True)
+    score = models.IntegerField(null=True, blank=True)
