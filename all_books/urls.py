3774d21d28366743c21ed3b5a43a2c70b5bcc2cc
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-08 13:38:37 +0530

    renewing and scoring upgraded in functionality

--- all_books/urls.py
+++ all_books/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('issue_requests_student/', views.IssueRequestStudentListView.as_view(), name='issue_requests_student'),
     path('update_book/<int:pk>/', views.BookUpdateView.as_view(), name='update_book'),
     path('book_request/<int:pk>/', book_request, name='book_request'),
-    path('return_book/<int:pk>/', return_book, name='return_book')
+    path('return_book/<int:pk>/', return_book, name='return_book'),
+    path('score_return/<int:pk>/', views.ScoreTheReturn.as_view(), name='score_return')
 ]
