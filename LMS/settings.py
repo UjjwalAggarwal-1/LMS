471c1af502d9a73ecd6f44ce54f520982394e59d
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-17 16:11:28 +0530

    mailing

--- LMS/settings.py
+++ LMS/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from keys import email_password
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -153,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '12aujjwalbis.aggarwal@gmail.com'
-EMAIL_HOST_PASSWORD = 'kllhvckikzjbqjgk'
+EMAIL_HOST_PASSWORD = email_password
 EMAIL_USE_TLS = True
