d0c8029dd272b06c157d14c612f29e41fa07e1ec
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-18 02:06:28 +0530

    setting updated

--- LMS/settings.py
+++ LMS/settings.py
@@ -9,10 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0w54ucwxluvtpmyhy_bjenq9rezw9@t9o2nnj)pn^2r!o@-@1i'
+SECRET_KEY = '51e12248adfe603dc187537af31318feacee8c39dc557a9b'
+#SECRET_KEY = 'django-insecure-0w54ucwxluvtpmyhy_bjenq9rezw9@t9o2nnj)pn^2r!o@-@1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lms-on-django.herokuapp.com']
 
