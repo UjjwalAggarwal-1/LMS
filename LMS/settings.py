0c04d9726f14fa83a50450b18bdab53040877888
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-19 02:43:20 +0530

    removed some app in settings

--- LMS/settings.py
+++ LMS/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     #'all_books',  #facing some duplicate error
     'allauth',
     'allauth.account',
