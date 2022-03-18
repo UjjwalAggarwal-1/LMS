e4ad1761c6c2ac3bd7d3109ff0f3752155370676
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-18 18:20:04 +0530

    site id changed

--- LMS/settings.py
+++ LMS/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-SITE_ID = 11
+SITE_ID = 12
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
