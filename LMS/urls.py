3e70236a3f7b813e6ec14e4de6f5dd3826d0c6bd
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-03-19 15:51:48 +0530

    minor feature updates

--- LMS/urls.py
+++ LMS/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from all_books.views import add_book, librarian_controls, cover, request_decision, reject_request, AddGenre
+from all_books.views import add_book, librarian_controls, cover, request_decision, reject_request, AddGenre, AllGenre, DeleteGenre
 from users import views as users_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -20,6 +20,8 @@ urlpatterns = [
     path('librarian_controls/<int:issue_request_id>/reject/', reject_request, name='reject_request'),
     path('add_book/', add_book, name='add_book'),
     path('add_genre/', AddGenre.as_view(), name='add_genre'),
+    path('all_genre/', AllGenre.as_view(), name='all_genre'),
+    path('<int:pk>/delete_genre/', DeleteGenre.as_view(), name='delete_genre'),
 ]
 
 
